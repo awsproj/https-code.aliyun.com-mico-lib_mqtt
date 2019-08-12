8595b4c5cbdc81995370d209a39501058337426f
丁全全 <dingqq@mxchip.com>
2019-08-12 13:34:07 +0800

    适配mxos

--- lib_mqtt.mk
+++ lib_mqtt.mk
@@ -10,8 +10,13 @@
 NAME := Lib_MQTT_AWS
 
 GLOBAL_INCLUDES := 	./include \
-					./platform \
 					./user_config
+
+ifeq ($(BUILD_MXOS), 1)
+GLOBAL_INCLUDES += 	./platform/mxos
+else
+GLOBAL_INCLUDES += 	./platform/mico
+endif
 
 $(NAME)_SOURCES := ./src/mqtt_client_common_internal.c \
 				   ./src/mqtt_client_connect.c \
@@ -19,8 +24,14 @@ $(NAME)_SOURCES := ./src/mqtt_client_common_internal.c \
 				   ./src/mqtt_client_subscribe.c \
 				   ./src/mqtt_client_unsubscribe.c \
 				   ./src/mqtt_client_yield.c \
-				   ./src/mqtt_client.c \
-				   ./platform/network_platform.c \
-				   ./platform/threads_platform.c \
-				   ./platform/timer_platform.c
-				   
+				   ./src/mqtt_client.c
+
+ifeq ($(BUILD_MXOS), 1)
+$(NAME)_SOURCES += ./platform/mxos/network_platform.c \
+				   ./platform/mxos/threads_platform.c \
+				   ./platform/mxos/timer_platform.c
+else
+$(NAME)_SOURCES == ./platform/mico/network_platform.c \
+				   ./platform/mico/threads_platform.c \
+				   ./platform/mico/timer_platform.c
+endif
